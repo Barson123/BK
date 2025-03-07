1f0cd74352bdf8ac8615d9f5733338f57f78ff52
Barson123 <pbsbarson@gmail.com>
2025-03-07 17:54:53 +0600

    script.js

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
-let canvas,classifier
-
-
-
+let canvas, classifier;
 function setup() {
-  canvas=createCanvas(280, 280);
-  background("white")
-  canvas.center()
+  canvas=createCanvas(400, 400);
+  background("white");
+  canvas.center();
   classifier = ml5.imageClassifier("DoodleNet",modelLoaded);
-  canvas.mouseReleased(classifyCanvas)
+  canvas.mouseReleased(classifyCanvas);
 }
-
 function classifyCanvas(){
-	classifier.classify(canvas,gotResult)
+  classifier.classify(canvas, gotResult);
 }
-
 function gotResult(result){
-	console.log(result)
+  console.log(result);
 }
-
 function modelLoaded(){
-	console.log("The model is loaded")
+  console.log("The model is loaded");
 }
 
 function draw() {
-
-	stroke("green")
-	strokezWeiight(13)
-	if (mouseIsPressed) {
-
-		line(pmouseX,pmouseY,mouseX,mouseY)
-	}
-  
+  stroke("green");
+  strokeWeight(13);
+  if (mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+  }
 }
 
 function clearCanvas(){
-	background("white")
+  background("white");
 }
