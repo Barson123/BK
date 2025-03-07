1bab3f724cbfbd5bb49d4ca53484990373efcd77
Barson123 <pbsbarson@gmail.com>
2025-03-07 17:21:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let canvas,classifier
+
+
+
+function setup() {
+  canvas=createCanvas(280, 280);
+  background("white")
+  canvas.center()
+  classifier = ml5.imageClassifier("DoodleNet",modelLoaded);
+  canvas.mouseReleased(classifyCanvas)
+}
+
+function classifyCanvas(){
+	classifier.classify(canvas,gotResult)
+}
+
+function gotResult(result){
+	console.log(result)
+}
+
+function modelLoaded(){
+	console.log("The model is loaded")
+}
+
+function draw() {
+
+	stroke("green")
+	strokezWeiight(13)
+	if (mouseIsPressed) {
+
+		line(pmouseX,pmouseY,mouseX,mouseY)
+	}
+  
+}
+
+function clearCanvas(){
+	background("white")
+}
